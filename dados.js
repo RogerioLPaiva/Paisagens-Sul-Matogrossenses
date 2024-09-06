ed7c745d818ee84c4ae652ded01dca362ef5006b
RogerioLPaiva <rogeriopaivanegocios@gmail.com>
2024-09-06 16:13:55 -0400

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,55 @@
+let dados =[
+    {
+        titulo : "Serra da Bodoquena",
+        descricao : "A Serra da Bodoquena, localizada no Mato Grosso do Sul, é um verdadeiro paraíso natural. Com suas cachoeiras cristalinas, rios exuberantes e trilhas que serpenteiam por meio da mata atlântica, a região encanta visitantes do mundo todo.",
+        link : "https://www.google.com.br/maps/place/Serra+da+Bodoquena/@-20.9999793,-56.8436545,15z/data=!3m1!4b1!4m6!3m5!1s0x947c76b30f916ec3:0x1fd1668e9224a58f!8m2!3d-21!4d-56.8333333!16s%2Fg%2F1tghqp17?hl=pt-BR&entry=ttu&g_ep=EgoyMDI0MDgyOC4wIKXMDSoASAFQAw%3D%3D",
+        tags: ["serra", "bodoquena", "mato grosso do sul", "natureza", "montanha", "trilha", "cachoeira", "rio", "mata atlântica", "ecoturismo"],
+
+    },
+    {
+        titulo: "Bonito",
+        descricao: "Conhecida pelas águas cristalinas e pela rica biodiversidade, Bonito é um dos destinos mais procurados para ecoturismo no Brasil. Mergulhos em rios, trilhas e grutas são algumas das atrações.",
+        link: "https://www.google.com.br/maps/place/Bonito,+Mato+Grosso+do+Sul,+Brazil",
+        tags: ["bonito", "mato grosso do sul", "ecoturismo", "mergulho", "rio", "trilha", "grutas", "águas cristalinas", "biodiversidade"],
+    },
+    {
+        titulo: "Pantanal",
+        descricao: "O Pantanal é a maior área úmida contínua do planeta e um dos biomas mais ricos em biodiversidade do mundo. A fauna e a flora são únicas e oferecem paisagens deslumbrantes.",
+        link: "https://www.google.com.br/maps/place/Pantanal,+Brazil",
+        tags: ["pantanal", "mato grosso do sul", "biodiversidade", "fauna", "flora", "safári", "ecoturismo"],
+    },
+    {
+        titulo: "Aquidauana",
+        descricao: "Aquidauana é conhecida por suas belas cachoeiras e pelo Parque Nacional da Serra da Bodoquena. A cidade oferece diversas opções de ecoturismo e aventura.",
+        link: "https://www.google.com.br/maps/place/Aquidauana,+Mato+Grosso+do+Sul,+Brazil",
+        tags: ["aquidauana", "mato grosso do sul", "cachoeira", "rio", "parque nacional", "ecoturismo", "aventura"],
+    },
+    
+    {
+        titulo: "Corumbá",
+        descricao: "Corumbá é uma cidade histórica com forte influência cultural paraguaia, localizada às margens do Rio Paraguai. Oferece um rico patrimônio histórico e cultural.",
+        link: "https://www.google.com.br/maps/place/Corumb%C3%A1,+Mato+Grosso+do+Sul,+Brazil",
+        tags:["corumbá", "mato grosso do sul", "história", "cultura", "patrimônio histórico", "rio paraguai"],
+    },
+    {
+        titulo: "Miranda",
+        descricao: "Cidade com rica história e cultura, localizada às margens do Rio Paraguai. Miranda oferece diversas opções de turismo de aventura e contato com a natureza.",
+        link: "https://www.google.com.br/maps/place/Miranda,+Mato+Grosso+do+Sul,+Brazil",
+        tags: ["miranda", "mato grosso do sul", "história", "cultura", "rio paraguai"],
+    },
+    {
+        titulo: "Parque Nacional da Serra da Bodoquena",
+        descricao: "Um dos mais importantes parques nacionais do Brasil, a Serra da Bodoquena abriga uma rica biodiversidade e paisagens deslumbrantes. Oferece diversas opções de trilhas, cachoeiras e observação da natureza.",
+        link: "https://www.google.com.br/maps/place/Parque+Nacional+da+Serra+da+Bodoquena,+Mato+Grosso+Sul,+Brazil",
+        tags:["parque nacional", "serra", "bodoquena", "mato grosso do sul", "natureza", "trilha", "camping"],
+    },
+    {
+        titulo: "Rio da Prata",
+        descricao: "Conhecido por suas águas cristalinas e pela rica biodiversidade, o Rio da Prata oferece uma das flutuações mais famosas de Bonito. Sua beleza natural e a possibilidade de observar peixes e plantas aquáticas atraem visitantes do mundo todo.",
+        link: "https://www.google.com/maps/place/Rio+da+Prata,+Mato+Grosso+do+Sul/data=!4m2!3m1!1s0x947ce71e0fa0cbc1:0x52773d41c2d33fd2?sa=X&ved=1t:242&ictx=111",
+        tags:["rio da prata", "bonito", "mato grosso do sul", "flutuação", "mergulho", "água cristalina"],
+    }
+];
+
+    
+ 
